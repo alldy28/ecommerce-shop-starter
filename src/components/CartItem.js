ab856621228c79ea9bf2990a01e889a71380cb87
alldy28 <aldimaulana19@gmail.com>
2023-08-31 13:07:49 +0700

    first commit

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,48 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import { IoMdAdd, IoMdClose, IoMdRemove } from "react-icons/io";
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ item }) => {
+  //destructure item
+  const { id, title, image, price, amount } = item;
+  return (
+    <div>
+      <div>
+        <div className="w-full min-h-[150px] flex items-center gap-x-4">
+          <Link>
+            {" "}
+            <img className="max-w-[80px]" src={image} alt="" />{" "}
+          </Link>
+          <div className="w-full flex flex-col">
+            <div div className="flex justify-between mb-2">
+              <div>
+                <Link to={`/product/${id}`}>{title}</Link>
+              </div>
+              <div className="text-xl cursor-pointer">
+                <IoMdClose className="text-gray-500 hover:text-red-500 transition" />
+              </div>
+            </div>
+            <div className=" flex gap-x-2 h-[36px] text-sm">
+            <div className="flex flex-1 max-w-[100px] bg-blue-400 items-center h-full border
+            text-primary font-medium">
+              <div div className = "flex-1 flex justify-center items-center cursor-pointer" >
+                <IoMdRemove />
+              </div>
+              <div className="h-full flex justify-center items-center px-2">
+                {amount}
+              </div>
+              <div className="flex-1 h-full flex justify-center items-center cursor-pointer">
+                <IoMdAdd />
+              </div>
+            </div>
+              <div>Item Price</div>
+              <div>Final Price</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,7 +1,36 @@
-import React from 'react';
+import React, {createContext, useState, useEffect} from 'react';
 
-const CartContext = () => {
-  return <div>CartContext</div>;
+
+//create context
+
+export const CartContext = createContext()
+
+const CartProvider = ({children}) => {
+  const [cart, setCart] = useState([]);
+
+  //add cart
+  const addToCart =  (product, id) => {
+    const newItem = {...product, amount: 1}
+    //cek jika item ada di keranjang
+    const cartItem = cart.find(item => {
+      return item.id === id;
+    });
+    //jika item ada di keranjang 
+    if (cartItem) {
+      const newCart = [...cart].map(item=>{
+        if (item.id === id) {
+          return {...item, amount: cartItem.amount + 1};
+        } else {
+          return item;
+        }
+      });
+      setCart(newCart);
+    } else {
+      setCart([...cart, newItem])
+    }
+  } 
+  console.log(cart)
+  return <CartContext.Provider value={{ cart, addToCart }}>{children}</CartContext.Provider>;
 };
 
-export default CartContext;
+export default CartProvider;
